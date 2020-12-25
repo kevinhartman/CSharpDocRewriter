13bc09675cb39d8d11891d7ed920f2e68b68c494
Kevin Hartman <kevin@hart.mn>
2020-12-25 02:49:39 -0500

    Tweak Vim settings to make editing easier.
    Specifically, aligns the sourcecode buffer so that the code element corresponding to the current comment starts on the first line.

--- CSharpDocRewriter/editor.sh
+++ CSharpDocRewriter/editor.sh
@@ -16,9 +16,26 @@ comment_temp_file="$(mktemp --suffix=.xml)"
 cat > "$comment_temp_file"
 
 # Open vim with source file and doc comment temp file.
+# There's a lot going on here...
+# Pseudocode:
+#   - Import predefined macros etc. from helpers.vimrc.
+#   - Disable scrolloff, used in next command to scroll source
+#     code buffer to the comment's code element.
+#   - Move to the source code buffer, jump to source element,
+#     scroll such that code element is first line in buffer,
+#     set the current line to the middle of the buffer view,
+#     so that enabling scrolloff won't cause an undesired scroll.
+#   - Enable scroll off to 999 to make the buffer scroll with
+#     any cursor movement (makes it easy for user to navigate
+#     the code element quickly).
+#   - Move back to the XML comment buffer for editing.
+#   - Disable XML auto-indenting.
 vim -o "$comment_temp_file" "$source_file" \
   -c 'source ./helpers.vimrc' \
-  -c 'exe "norm \<C-w>j'$element_line_number'Gzt\<C-w>w"' \
+  -c 'set so=0' \
+  -c 'exe "norm \<C-w>j'$(($element_line_number + 1))'GztM"' \
+  -c 'set so=999' \
+  -c 'exe "norm \<C-w>w"' \
   -c 'setl noai nocin nosi inde=' \
   </dev/tty >/dev/tty
 
